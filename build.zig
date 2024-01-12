95aab207907e7abf554c3ae4e699f6b20852c017
kamlesh-nb <kamlesh.bambarde@outlook.com>
2024-01-12 10:39:05 +0530

    first commit

---
+++ build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "datetime",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    _ = b.addModule("datetime", .{
+        .root_source_file = .{ .path = "src/main.zig" },
+    });
+
+    b.installArtifact(lib);
+}
